f5c8f605256f67de98c13517ba04b8eb81a447f3
enncy <877526278@qq.com>
2024-07-12 23:53:47 +0800

    打包时优化MD上方时间信息

--- src/default-plugins/time_writer.ts
+++ src/default-plugins/time_writer.ts
@@ -1,5 +1,6 @@
-import { MarkdownContext, Plugin } from '../interface';
+import { FileInfo, MarkdownContext, Plugin } from '../interface';
 import fs from 'fs';
+import { parseMarkdownContext } from '../utils';
 
 export default class TimeWriterPlugin implements Plugin {
 	onMarkdownChange(filepath: string, ctx: MarkdownContext): void {
@@ -7,5 +8,17 @@ export default class TimeWriterPlugin implements Plugin {
 			ctx.metadata.create_at = fs.statSync(filepath).birthtime.toLocaleString();
 		}
 		ctx.metadata.update_at = new Date().toLocaleString();
+	}
+	onRenderFinish(file_info: FileInfo[]) {
+		for (const info of file_info) {
+			const ctx = info.markdown_context;
+			if (!!ctx.metadata.create_at === false) {
+				ctx.metadata.create_at = fs.statSync(info.filepath).birthtime.toLocaleString();
+			}
+			ctx.metadata.create_at = new Date(info.create_at).toLocaleString();
+			const md_content = parseMarkdownContext(ctx);
+			// 自动更新文件信息
+			fs.writeFileSync(info.filepath, md_content);
+		}
 	}
 }
