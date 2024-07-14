e7f00bab2a30e18b0f119417337479530f5f3ab8
enncy <877526278@qq.com>
2024-07-14 12:03:56 +0800

    添加侧边栏，顶部栏，以及底部拓展功能“

--- plugins/custom-readme/index.js
+++ plugins/custom-readme/index.js
@@ -79,15 +79,67 @@ function buildFile(
         script.innerHTML = `window.__ewiki_files_info__ = JSON.parse(decodeURIComponent("${encodeURIComponent(JSON.stringify(raw_infos))}"));`;
         document.head.append(script);
 
-
-        const sidebar = document.querySelector('.sidebar')
-        if (!sidebar) {
-            return
-        }
         // 添加样式
         const style = document.createElement('style')
         style.textContent = style_content
         document.head.append(style)
+
+
+        // 添加顶部额外链接信息
+        const header_extra_el = document.querySelector('.header .extra')
+        const header_links = EWiki.config?.["custom-readme-plugin"]?.["header-links"] || []
+        if (header_extra_el) {
+            addExtraLinks(document, header_extra_el, header_links)
+        } else {
+            if (header_links.length > 0) {
+                console.log('[custom-readme] [warning] : header extra element not found, please add a div with class "extra" in ".header')
+            }
+        }
+
+
+
+        // 添加底部组件 
+        const footer_el = document.querySelector('.footer')
+        const footer_template = EWiki.config?.["custom-readme-plugin"]?.["footer-template"] || ''
+        if (footer_template) {
+            if (footer_el) {
+                let content = fs.readFileSync(footer_template, { encoding: 'utf-8' }).toString()
+                const metadata = readme_info.markdown_context.metadata
+                for (const key in metadata) {
+                    if (Object.hasOwnProperty.call(metadata, key)) {
+                        content = content.replace(new RegExp(`{{METADATA.${key}}}`, 'g'), metadata[key])
+                    }
+                }
+                footer_el.innerHTML = content
+            } else {
+                console.log('[custom-readme] [warning] : footer element not found, please add a div with class "footer" in ".markdown-wrapper')
+            }
+        }
+
+        // 以下是侧边栏操作
+        const sidebar = document.querySelector('.sidebar')
+        if (!sidebar) {
+            return
+        }
+
+        // 添加侧边栏额外链接信息
+        const sidebar_extra_el = document.querySelector('.sidebar-wrapper .extra')
+        const sidebar_links_group = EWiki.config?.["custom-readme-plugin"]?.["sidebar-links-group"] || []
+        if (sidebar_extra_el) {
+            for (const group of sidebar_links_group) {
+                const group_el = document.createElement('div')
+                group_el.setAttribute('class', 'links-group')
+                const group_name = document.createElement('div')
+                group_name.textContent = group.name
+                group_el.append(group_name, document.createElement('hr'))
+                sidebar_extra_el.appendChild(group_el)
+                addExtraLinks(document, group_el, group.links)
+            }
+        } else {
+            if (sidebar_links_group.length > 0) {
+                console.log('[custom-readme] [warning] : sidebar extra element not found, please add a div with class "extra" in ".sidebar-wrapper"')
+            }
+        }
 
 
         // 添加首页链接
@@ -156,6 +208,30 @@ function buildFile(
         })
     })
 
+}
+
+function addExtraLinks(document, container, links) {
+    for (const hl of links) {
+        const a = document.createElement('a')
+        a.setAttribute('href', hl.href || '')
+        a.setAttribute('target', hl.target || '_blank')
+        a.setAttribute('style', hl.style || '')
+        Object.assign(a, hl.attrs || {})
+
+        if (hl.text) {
+            a.textContent = hl.text.trim()
+        } else if (hl.html) {
+            a.innerHTML = hl.html.trim()
+        } else if (hl.img) {
+            const img = document.createElement('img')
+            img.setAttribute('src', hl.img.src.trim() || '')
+            img.setAttribute('style', hl.img.style || '')
+            a.appendChild(img)
+        } else {
+            console.log('[custom-readme] [warning] : link must have text|html|img property')
+        }
+        container.appendChild(a)
+    }
 }
 
 
